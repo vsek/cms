06340013273e1bf7499ae296c32f76e5dbecfca5
Václav Stodůlka <vsek@seznam.cz>
2015-12-01 23:24:41 +0100

    Upraven základní presenter

---
+++ app/AdminModule/presenters/BasePresenter.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\AdminModule\Presenters;
+/**
+ * Description of BasePresenter
+ *
+ * @author vsek
+ */
+class BasePresenter extends BasePresenterM{
+    //put your code here
+}
