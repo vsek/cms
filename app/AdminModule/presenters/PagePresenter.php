20273b32a1cb641234c6f84b848c7a10a9a78296
Václav Stodůlka <vsek@seznam.cz>
2016-07-11 15:00:16 +0200

    Možnost upravit si správu stránek

---
+++ app/AdminModule/presenters/PagePresenter.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\AdminModule\Presenters;
+
+/**
+ * Description of PagePresenter
+ *
+ * @author vsek
+ */
+class PagePresenter extends PagePresenterM{
+    //put your code here
+}
