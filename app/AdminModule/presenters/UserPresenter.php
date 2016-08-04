4da0e120fb0073714b193cf57ca51502a0736b53
Václav Stodůlka <vsek@seznam.cz>
2016-08-04 12:43:24 +0200

    Úprava pro možnost předělat si uživatele

---
+++ app/AdminModule/presenters/UserPresenter.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\AdminModule\Presenters;
+
+/**
+ * Description of UserPresenter
+ *
+ * @author vsek
+ */
+class UserPresenter extends UserPresenterM{
+    //put your code here
+}
