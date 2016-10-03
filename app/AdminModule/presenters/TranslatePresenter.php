3e1d6f99d6afb0aacae6af8834ea20ffe7562eb3
Václav Stodůlka <vsek@seznam.cz>
2016-10-03 12:33:00 +0200

    Úprava překladů

---
+++ app/AdminModule/presenters/TranslatePresenter.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\AdminModule\Presenters;
+
+/**
+ * Description of TranslatePresenter
+ *
+ * @author vsek
+ */
+class TranslatePresenter extends TranslatePresenterM{
+    //put your code here
+}
