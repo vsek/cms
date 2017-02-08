e390308247d43354c2b824f22874db45f703a47b
Václav Stodůlka <vsek@seznam.cz>
2017-02-08 11:45:16 +0100

    Přidána možnost vlastního EmailPresenteru

---
+++ app/AdminModule/presenters/EmailPresenter.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\AdminModule\Presenters;
+
+/**
+ * Description of EmailPresenter
+ *
+ * @author Vsek
+ */
+class EmailPresenter extends EmailPresenterM{
+    //put your code here
+}
