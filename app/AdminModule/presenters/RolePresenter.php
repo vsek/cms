7b89b96d10fb92beeaeb27a68976aa405f002516
Václav Stodůlka <vsek@seznam.cz>
2017-08-01 07:51:15 +0200

    Možnost dědit RolePresenter

---
+++ app/AdminModule/presenters/RolePresenter.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Vsek
+ * Date: 01.08.2017
+ * Time: 7:46
+ */
+
+namespace App\AdminModule\Presenters;
+
+
+class RolePresenter extends RolePresenterM
+{
+
+}
